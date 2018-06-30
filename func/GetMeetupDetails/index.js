9c63ab31beb611b4fa39adf1257931b54eb058ac
Daniel Oliver <danieloliversoftware@gmail.com>
2018-06-30 18:42:53 -0400

    working data queries

--- func/GetMeetupDetails/index.js
+++ func/GetMeetupDetails/index.js
@@ -5,9 +5,9 @@ var request = require('request');
 module.exports = function (context, meetupToAdd) {
     var tableSvc = azure.createTableService();
     var meetupUrl = `https://api.meetup.com/${meetupToAdd.name}?key=${process.env.meetup_api_key}&sign=true`
-    request(meetupUrl, function (error, response, body) {        
+    request(meetupUrl, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            const item = JSON.parse(body);     
+            const item = JSON.parse(body);
             const entGen = azure.TableUtilities.entityGenerator;
             const newTask = {
                 PartitionKey: entGen.String(meetupToAdd.area),
@@ -20,9 +20,10 @@ module.exports = function (context, meetupToAdd) {
                 MembersCount: entGen.Int32(item.members),
                 FullName: entGen.String(item.name),
                 Link: entGen.String(item.link),
-                UrlName: entGen.String(item.urlname)
-              };
-            tableSvc.mergeEntity('meetup', newTask, function(error, result, response){ if(!error) { }});
+                UrlName: entGen.String(item.urlname),
+                LastQueriedUTC: entGen.DateTime(moment())
+            };
+            tableSvc.mergeEntity('meetup', newTask, function (error, result, response) { if (!error) { } });
         }
         context.done();
     })
