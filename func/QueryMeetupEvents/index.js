b4e4db74f5d87e5f7d6f13791c3d9e40dc176e91
Daniel Oliver <danieloliversoftware@gmail.com>
2018-06-30 17:04:24 -0400

    parse json

--- func/QueryMeetupEvents/index.js
+++ func/QueryMeetupEvents/index.js
@@ -5,9 +5,10 @@ var request = require('request');
 module.exports = function (context, meetupToRefresh) {
     var tableSvc = azure.createTableService();
     var meetupUrl = `https://api.meetup.com/${meetupToRefresh.name}/events?key=${process.env.meetup_api_key}&sign=true&status=past&no_earlier_than=${moment(meetupToRefresh.LastQueriedUTC).format("YYYY-MM-DD")}T00:00:00&status=past`
-    request(meetupUrl, function (error, response, body) {
+    request(meetupUrl, function (error, response, body) {        
         if (!error && response.statusCode == 200) {
-            console.log(body) // Print the google web page.
+            const items = JSON.parse(body);
+            console.log(items);
         }
         context.done();
     })
