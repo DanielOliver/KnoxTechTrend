bb7a09eb0dce7510b8be3aadfc875b4e7a4ed35f
Daniel Oliver <danieloliversoftware@gmail.com>
2018-08-14 21:44:16 -0400

    use https build.

--- func/NightlyRebuild/index.js
+++ func/NightlyRebuild/index.js
@@ -1,16 +1,18 @@
-var http = require('http');
+var https = require('https');
 
 module.exports = function (context, myTimer) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var options = {
-        host: 'https://ci.appveyor.com/api/builds',
+        host: 'ci.appveyor.com',
+        path: '/api/builds',
         method: 'POST',
         headers: {
             Authorization: `Bearer ${process.env.appveyor_api_key}`,
             'content-type': 'application/json'
-        },
-        body: {
+        }
+    }
+    var body = {
             "accountName": "DanielOliver",
             "projectSlug": "KnoxTechTrend",
             "branch": process.env.appveyor_branch,
@@ -20,21 +22,29 @@ module.exports = function (context, myTimer) {
             }
         }
 
-    };
-
     // Set up the request
-    var req = http.request(options, (res) => {
+    var req = https.request(options, (res) => {
+        context.log(`STATUS: ${res.statusCode}`)
+        context.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+        res.setEncoding('utf8')
+        
+        res.on('data', (d) => {
+            context.log(`Data: ${d}`)
+        });
         res.on("end", () => {
             context.log("done")
-            context.done();
+            context.done()
         });
     }).on("error", (error) => {
-        context.log('error');
+        context.log('error')
+        context.log(error)
         context.res = {
             status: 500,
             body: error
         };
-        context.done();
+        context.done()
     });
-    req.end();
+
+    req.write(JSON.stringify(body))
+    req.end()
 };
