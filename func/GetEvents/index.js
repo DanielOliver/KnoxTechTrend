db6d711bfafa3a44730d01a3502778585b0e39ca
Daniel O <danieleugeneoliver@gmail.com>
2018-08-15 19:02:44 -0400

    Refactor date.

--- func/GetEvents/index.js
+++ func/GetEvents/index.js
@@ -14,20 +14,24 @@ module.exports = function (context, myTimer) {
     tableSvc.queryEntities('meetup', query, null, function (error, result, response) {
         if (!error) {
             result.entries.forEach(value => {
+
+                var lastQueriedUtc = moment((value.LastQueriedUTC || { _: '2015-01-01T00:00:00.000' })._ || '2015-01-01T00:00:00.000').toDate();
+                var lastEventsQueriedUTC = moment((value.LastEventsQueriedUTC || { _: '2015-01-01T00:00:00.000' })._ || '2015-01-01T00:00:00.000').toDate();
+                                    
                 var message = {
                     area: value.PartitionKey._,
                     name: value.RowKey._,
-                    LastQueriedUTC: moment((value.LastQueriedUTC || { _: '2015-01-01T00:00:00.000' })._ || '2015-01-01T00:00:00.000').toDate(),
-                    LastEventsQueriedUTC: moment((value.LastEventsQueriedUTC || { _: '2015-01-01T00:00:00.000' })._ || '2015-01-01T00:00:00.000').toDate(),
-                    LastQueriedLocal: (value.Timezone != null && value.LastQueriedUTC != null ? moment_tz().tz(value.LastQueriedUTC, value.Timezone).toDate() : null),
-                    LastEventsQueriedLocal: (value.Timezone != null && value.LastEventsQueriedUTC != null ? moment_tz().tz(value.LastEventsQueriedUTC, value.Timezone).toDate() : null),
+                    LastQueriedUTC: lastQueriedUtc,
+                    LastEventsQueriedUTC: lastEventsQueriedUTC,
+                    LastQueriedLocal: (value.Timezone != null ? moment_tz().utc(value.LastQueriedUTC).tz(value.Timezone).toDate() : lastQueriedUtc),
+                    LastEventsQueriedLocal: (value.Timezone != null ? moment_tz().utc(value.LastEventsQueriedUTC).tz(value.Timezone).toDate() : lastEventsQueriedUTC),
                     Timezone: (value.Timezone || 'US/Eastern')
                 };
-                if (moment().subtract('30', 'day') >= moment(message.LastQueriedUTC)) {
+                if (moment_tz().utc().subtract('30', 'day') >= moment(message.LastQueriedUTC)) {
                     context.log(`Refreshing meetup: ${value.RowKey._}.`);
                     queueSvc.createMessage('meetup-add-details', JSON.stringify(message).trim(), function (error) { if (!error) { } });
                 }
-                if (moment().subtract('10', 'hours') >= moment(message.LastEventsQueriedUTC)) {
+                if (moment_tz().utc().subtract('10', 'hours') >= moment(message.LastEventsQueriedUTC)) {
                     context.log(`Refreshing events for meetup: ${value.RowKey._}.`);
                     queueSvc.createMessage('meetup-refresh', JSON.stringify(message).trim(), function (error) { if (!error) { } });
                 }
