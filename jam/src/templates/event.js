2d2cbcb1c1612ef8a9ee2dfef0c46bc3300632c4
Daniel Oliver <danieloliversoftware@gmail.com>
2018-08-18 13:01:46 -0400

    add maps

--- jam/src/templates/event.js
+++ jam/src/templates/event.js
@@ -2,17 +2,67 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from '../components/layout'
 import { Link } from 'gatsby'
+import { Helmet } from 'react-helmet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import Leaflet from 'leaflet'
+
+class LeafletMap extends React.Component {
+    render() {
+        const greenIcon = new Leaflet.Icon({
+            iconUrl: require('leaflet/dist/images/marker-icon.png'),
+            iconSize:     [25, 41]
+        });
+
+        const position = [  this.props.latitude, this.props.longitude ];
+        const venueName = this.props.venueName;
+
+        return  (
+            <Map center={position} zoom={14} style={{ "height": "400px", "width": "400px" }}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+                <Marker position={position} icon={greenIcon} >
+                    <Popup>
+                        <span>{venueName}</span>
+                    </Popup>
+                </Marker>
+            </Map>
+        )
+    }
+}
 
 class EventTemplate extends React.Component {
+    getMap(latitude, longitude, venueName) {
+        if(latitude == null || longitude == null) {
+            return (
+                <p>No Location Information</p>
+            )
+        } else {
+            return (
+                <LeafletMap longitude={longitude} latitude={latitude} venueName={venueName} />
+            )
+        }
+    }
+
     render() {
         const meetup = this.props.data.meetup
         const event = this.props.data.event
         return (
             <Layout>
                 <div>
+                    <Helmet>
+                        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.3/dist/leaflet.css"
+                            integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+                            crossorigin=""/>
+                        <script src="https://unpkg.com/leaflet@1.3.3/dist/leaflet.js"
+                            integrity="sha512-tAGcCfR4Sc5ZP5ZoVz0quoZDYX5aCtEm/eu1KhSLj2c9eFrylXZknQYmxUssFaVJKvvc0dJQixhGjG2yXWiV9Q=="
+                            crossorigin=""></script>
+                    </Helmet>
                     <h1><Link to={meetup.trendURL}>{meetup.FullName}</Link></h1>
                     <h2>{event.Name}</h2>
                     <p>{event.MeetupDateLocal}</p>
+                    { this.getMap(event.VenueLatitude, event.VenueLongitude, event.VenueName) }
                 </div>
             </Layout>
         )
@@ -33,6 +83,9 @@ export const pageQuery = graphql`
             RowKey
             MeetupDateLocal(formatString: "MMMM DD, YYYY")
             Link
+            VenueLongitude
+            VenueLatitude
+            VenueName
         }
     }
 `
