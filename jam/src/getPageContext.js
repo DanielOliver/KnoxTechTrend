6ecb2f18c9a4996b52baccc458a46386c75499ac
Daniel Oliver <danieloliversoftware@gmail.com>
2018-09-03 17:48:15 -0400

    redo colors

--- jam/src/getPageContext.js
+++ jam/src/getPageContext.js
@@ -2,22 +2,22 @@
 
 import { SheetsRegistry } from 'jss';
 import { createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
+import blue from '@material-ui/core/colors/blue';
+import grey from '@material-ui/core/colors/grey';
 
 // A theme with custom primary and secondary color.
 // It's optional.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700],
+      light: grey[50],
+      main: grey[400],
+      dark: grey[600],
     },
     secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700],
+      light: blue[300],
+      main: blue[500],
+      dark: blue[700],
     },
   },
 });
