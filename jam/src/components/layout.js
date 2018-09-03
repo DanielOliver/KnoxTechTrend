e5252f1226d1e9d472eae43618fbe3fc5bb86a0c
Daniel Oliver <danieloliversoftware@gmail.com>
2018-09-03 17:54:47 -0400

    redo keyword

--- jam/src/components/layout.js
+++ jam/src/components/layout.js
@@ -22,8 +22,8 @@ const Layout = ({ children, data }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'Knox Tech Trend' },
+            { name: 'keywords', content: 'knoxville, tech, trend' },
           ]}
         />
         <Header siteTitle={data.site.siteMetadata.title}>
