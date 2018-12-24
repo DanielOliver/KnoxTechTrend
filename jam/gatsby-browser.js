917778366e64aefe355655e891516d9e7187475d
Daniel Oliver <danieloliversoftware@gmail.com>
2018-12-24 04:12:52 -0500

    wrapRootElement

--- jam/gatsby-browser.js
+++ jam/gatsby-browser.js
@@ -4,10 +4,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Layout from "./src/components/layout"
 
-export const wrapPageElement = ({ element, props }) => {
+export const wrapRootElement = ({ element }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
-  return (<Layout {...props}>{element}</Layout>)
+  return (<Layout>{element}</Layout>)
 }
 
 // export const wrapRootElement = ({ element }) => {
